105844fcb7ed4f28bdebf7e46a5e13ca71774989
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-06-29 16:40:19 +0200

    Added CPU cgroups for service config

--- single-server/text-service.sh
+++ single-server/text-service.sh
@@ -5,4 +5,5 @@ docker run -d --name text-service -p 8081:8080 \
     -e DB_HOST='crazerace-db' -e DB_PORT='5432' \
     -e DB_USERNAME='textservice' -e DB_NAME='texts' \
     -e DB_PASSWORD=$TEXT_SERVICE_DB_PASSWORD \
+    --memory=300m --cpus="0.2" \
     eu.gcr.io/crazerace/text-service:1.0-RC
--- single-server/user-service.sh
+++ single-server/user-service.sh
@@ -6,4 +6,6 @@ docker run -d --name user-service -p 8080:8080 \
     -e DB_USERNAME='userservice' -e DB_NAME='directory' \
     -e DB_PASSWORD=$USER_SERVICE_DB_PASSWORD \
     -e JWT_SECRET=$JWT_SECRET \
+    -e PASSWORD_PEPPER=$PASSWORD_PEPPER \
+    --memory=300m --cpus="0.2" \
     eu.gcr.io/crazerace/user-service:1.0-RC
