310d2a60caae3ae3f494a2c39ff206fe340b908f
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-07-09 00:17:40 +0200

    Fixed conflicts in httplogger

--- single-server/httplogger.sh
+++ single-server/httplogger.sh
@@ -3,8 +3,4 @@
 docker run -d --name httplogger -p 8083:8080 \
     --restart always --network crazerace \
     --memory=50m --cpus="0.1" \
-<<<<<<< HEAD
     czarsimon/httplogger:0.3
-=======
-    czarsimon/httplogger:0.2
->>>>>>> ccc2799289b6f7e7bd5eb67f71120877feb60d0f
