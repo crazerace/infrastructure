4db8665584955ed43eea9b4e08459fdb405653a6
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-08-15 00:28:55 +0200

    Updated httplogger

--- single-server/httplogger.sh
+++ single-server/httplogger.sh
@@ -3,4 +3,4 @@
 docker run -d --name httplogger -p 8083:8080 \
     --restart always --network crazerace \
     --memory=50m --cpus="0.1" \
-    czarsimon/httplogger:0.3
+    czarsimon/httplogger:0.4
